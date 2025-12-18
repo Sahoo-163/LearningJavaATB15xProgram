1f3cdb2a115d8a0bf869a5f3d0eb07383688c6e6
Chinmay-163 <chinmay.sahoo@voltmoney.in>
2025-12-18 16:39:35 +0530

    My code 1st push commit

---
+++ src/ex_01_Java_Basics/Lab_001_HelloWorld.java
@@ -0,0 +1,8 @@
+package ex_01_Java_Basics;
+
+public class Lab_001_HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world");
+    }
+}
---
+++ src/ex_01_Java_Basics/Lab_002_HelloWorld2.java
@@ -0,0 +1,7 @@
+package ex_01_Java_Basics;
+
+public class Lab_002_HelloWorld2 {
+    public static void main(String[] args) {
+        System.out.println("This is a comment");
+    }
+}
